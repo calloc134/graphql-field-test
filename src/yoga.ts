ce3d837dc5c8e9213edd05806052cb250dfecbf1
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-05-20 11:23:42 +0900

    試行錯誤

--- src/yoga.ts
+++ src/yoga.ts
@@ -1,3 +1,4 @@
+import { Kind, parse } from "graphql";
 import { createSchema, createYoga } from "graphql-yoga";
 
 // available when handling requests, needs to be provided by the implementor
@@ -18,7 +19,26 @@ export const yoga = createYoga<ServerContext>({
     resolvers: {
       Query: {
         greetings: (_parent, _args, _context, _info) => {
-          console.log(`${_context.params.query}`);
+          const parsedQuery = parse(
+            _context.params.query?.replace("\n", "") || "{}"
+          );
+
+          // console.dir(parsedQuery, { depth: null });
+
+          const thisQueryDefinition = parsedQuery.definitions.find(
+            (definition) => {
+              if (definition.kind === Kind.OPERATION_DEFINITION) {
+                return definition;
+              }
+            }
+          );
+
+          console.dir(thisQueryDefinition, { depth: null });
+
+          // const queryAST = getOperationAST(parsedQuery.definitions, "query");
+
+          // console.dir(document, { depth: null });
+
           // console.log(`_info: ${JSON.stringify(_info)}`);
           // console.log(`_parent: ${JSON.stringify(_parent)}`);
           // console.log(`_args: ${JSON.stringify(_args)}`);
